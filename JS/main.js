9c67c789441512e5604318b497adadaa2c308655
Andrew Dank <adank2011@gmail.com>
2016-12-14 16:03:32 -0800

    updated the game mechanics and display

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@ var deck;
 var playerDecks;
 var wonDecks;
 var battleCards;
+var saveWins;
 var winMessage;
 
 
@@ -14,21 +15,32 @@ function initialize() {
   shuffleDeck();
   dealPlayerCards();
   wonDecks = [[], []];
-  battleCards = [null, null];
+  saveWins = [0, 0];
   winMessage = null;
   render();
 }
 
 function render() {
-  renderPlayersWonCount();
+  if (wonDecks) renderPlayersWonStack();
   renderPlayersDecks();
-  renderBattleCards();
+  if (battleCards) renderBattleCards();
 }
 
-function renderPlayersWonCount() {
-  var wonElems = document.querySelectorAll('.card.won p');
-  wonElems[0].innerHTML = wonDecks[0].length;
-  wonElems[1].innerHTML = wonDecks[1].length;
+function renderPlayersWonStack() {
+  var wonElems = document.querySelectorAll('.card.won');
+  var cardClass;
+  if (wonDecks[0].length > saveWins[0]) {
+    cardClass = 'card won ' + wonDecks[0][wonDecks[0].length - 1 - saveWins[0]].suit + wonDecks[0][wonDecks[0].length - 1- saveWins[0]].name;
+    wonElems[0].className = cardClass;
+  } else {
+    wonElems[0].className = 'card won';
+  }
+  if (wonDecks[1].length > saveWins[1]) {
+    cardClass = 'card won ' + wonDecks[1][wonDecks[1].length - 1 - saveWins[1]].suit + wonDecks[1][wonDecks[1].length - 1 - saveWins[1]].name;
+    wonElems[1].className = cardClass;
+  } else {
+    wonElems[1].className = 'card won';
+  }
 }
 
 function renderPlayersDecks() {
@@ -38,7 +50,11 @@ function renderPlayersDecks() {
 }
 
 function renderBattleCards() {
-
+  var battleElems = document.querySelectorAll('.battle .card');
+  var cardClass = 'card ' + battleCards[0][0].suit + battleCards[0][0].name;
+  battleElems[0].className = cardClass;
+  cardClass = 'card ' + battleCards[1][0].suit + battleCards[1][0].name;
+  battleElems[1].className = cardClass;
 }
 
 function renderPlayerDeck(deck, player) {
@@ -109,21 +125,38 @@ document.getElementById('battle-btn').addEventListener('click', handleBattleClic
 
 function handleBattleClick() {
   battleCards = [[], []];
+  saveWins = [0, 0];
   battleCards[0].push(playerDecks[0].pop());
   battleCards[1].push(playerDecks[1].pop());
   player1value = battleCards[0][0].value;
   player2value = battleCards[1][0].value;
   if (player1value > player2value) {
     wonDecks[0].push(battleCards[0][0], battleCards[1][0]);
+    saveWins[0] = 2;
   } else if (player1value < player2value) {
     wonDecks[1].push(battleCards[0][0], battleCards[1][0]);
+    saveWins[1] = 2;
   } else {
     // it's war!
     console.log('go to war');
   }
   winnerCheck();
-  console.log(battleCards[0], battleCards[1])
-  console.log(wonDecks[0], wonDecks[1])
+  if (!winMessage) flipCheck();
+  render();
+  console.log(playerDecks[0], playerDecks[1])
+}
+
+function flipCheck() {
+  if (playerDecks[0].length === 0) {
+    console.log('flipping 0')
+    playerDecks[0] = wonDecks[0];
+    wonDecks[0] = [];
+  }
+  if (playerDecks[1].length === 0) {
+    console.log('flipping 1')
+    playerDecks[1] = wonDecks[1];
+    wonDecks[1] = [];
+  }
 }
 
 function winnerCheck(){
